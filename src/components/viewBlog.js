d86394919c28692397e5d2d1cb6506232d013d01
Jatin Sharma <mca22.jatin.sharma@sunstone.edu.in>
2023-07-23 12:40:02 +0530

    inserted Likes and Dislike Feature

--- src/components/viewBlog.js
+++ src/components/viewBlog.js
@@ -3,13 +3,16 @@ import Navbar from "./navbar";
 
 const ViewBlog = () => {
   let [a, setA] = useState();
-  const Blogkey = localStorage.getItem("Blogkey");
+
+  let Blogkey = localStorage.getItem("Blogkey");
   a = JSON.parse(localStorage.getItem("AllBlogs")).filter((blog) => {
     return blog.id == Blogkey;
   });
-  const requiredBlog = a[0];
-  console.log("this is", requiredBlog);
 
+  const requiredBlog = a[0];
+  let lik = requiredBlog.likes;
+  let dislik = requiredBlog.dislikes;
+  console.log("this is", requiredBlog);
   const callAuthor = () => {
     return requiredBlog.userName;
   };
@@ -22,15 +25,41 @@ const ViewBlog = () => {
   const callCategory = () => {
     return requiredBlog.blogCategory;
   };
-  const callLikes = () => {
-    return requiredBlog.likes;
+
+  const updateBlogNumber = () => {
+    requiredBlog.likes = lik;
+    const newBlogs = JSON.parse(localStorage.getItem("AllBlogs")).map(
+      (blog) => {
+        if (blog.key == localStorage.getItem(Blogkey)) {
+          blog.likes = lik;
+          return blog;
+        } else {
+          return blog;
+        }
+      }
+    );
+    localStorage.setItem("AllBlogsss", JSON.stringify(newBlogs));
   };
-  const callDislikes = () => {
-    return requiredBlog.dislikes;
+
+  const updateBlogNumberDislike = () => {
+    requiredBlog.dislikes = dislik;
+    const newBlogs = JSON.parse(localStorage.getItem("AllBlogs")).map(
+      (blog) => {
+        if (blog.key == localStorage.getItem(Blogkey)) {
+          blog.likes = dislik;
+          return blog;
+        } else {
+          return blog;
+        }
+      }
+    );
+    localStorage.setItem("AllBlogsss", JSON.stringify(newBlogs));
   };
+
   const callComments = () => {
     return requiredBlog.comments.length;
   };
+
   return (
     <>
       <Navbar />
@@ -44,8 +73,33 @@ const ViewBlog = () => {
         </p>
         <main>{callDesc()}</main>
         <footer>
-          <span>Likes : {callLikes()}</span>
-          <span>Dislike : {callDislikes()}</span>
+          <span>
+            <span
+              className="material-symbols-outlined"
+              style={{ fontSize: "5rem", cursor: "pointer" }}
+              onClick={() => {
+                console.log("akaca");
+                document.querySelector("#LikeValue").textContent = ++lik;
+                updateBlogNumber();
+              }}
+            >
+              thumb_up
+            </span>
+            : <span id="LikeValue">0</span>
+          </span>
+          <span>
+            <span
+              className="material-symbols-outlined"
+              style={{ fontSize: "5rem" }}
+              onClick={() => {
+                document.querySelector("#DisLikeValue").textContent = ++dislik;
+                updateBlogNumberDislike();
+              }}
+            >
+              thumb_down
+            </span>
+            : <span id="DisLikeValue">0</span>
+          </span>
           <span>Comment : {callComments()}</span>
         </footer>
       </section>
